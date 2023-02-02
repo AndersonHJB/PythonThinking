fd9272de88e81649badec381c90661af2ab9b16c
AI悦创 <cleland1432803776@icloud.com>
2023-02-02 09:52:57 +0800

    init

---
+++ chapter01/lesson01.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/2/1 10:14
+# @Author  : AI悦创
+# @FileName: lesson01.py
+# @Software: PyCharm
+# @Blog    ：https://bornforthis.cn/
+import pygame  # 导入 pygame 库
+
+# 进入游戏时需要加载游戏——可理解为：游戏初始化
+pygame.init()  # 调用初始化函数
+
+
+
